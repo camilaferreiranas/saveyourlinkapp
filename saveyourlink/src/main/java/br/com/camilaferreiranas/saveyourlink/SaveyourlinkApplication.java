d87c441c7dbc2b27a16130743c765b9a41e1c3bb
Camila Ferreira <camila_fn97@hotmail.com>
2024-03-27 13:32:24 -0300

    feat: Commit inicial

---
+++ saveyourlink/src/main/java/br/com/camilaferreiranas/saveyourlink/SaveyourlinkApplication.java
@@ -0,0 +1,13 @@
+package br.com.camilaferreiranas.saveyourlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveyourlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveyourlinkApplication.class, args);
+	}
+
+}
---
+++ saveyourlink/src/test/java/br/com/camilaferreiranas/saveyourlink/SaveyourlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.camilaferreiranas.saveyourlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveyourlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
